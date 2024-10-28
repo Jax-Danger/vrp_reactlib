8925b05798c939d0468568e5c54a71978da008bf
Jax-Danger <jax@jaxdanger.com>
2024-10-27 22:55:33 -0400

    Delete server_vrp.lua

--- server/server_vrp.lua
+++
@@ -1,7 +0,0 @@
-local Tunnel = module("vrp", "lib/Tunnel")
-local Proxy = module("vrp", "lib/Proxy")
-local vRP = Proxy.getInterface("vRP")
-
-async(function()
-  vRP.loadScript("vrp_inventory", "server/server")
-end)
